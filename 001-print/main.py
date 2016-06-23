72709ac65215add49fde70522de5a9caa95aa6a7
Takahiro Yamashita <takahiro.yamashita@uzabase.com>
2016-06-23 14:48:37 +0900

    raw program main.py

---
+++ 001-print/main.py
@@ -0,0 +1,9 @@
+# 行頭に # を書くと、プログラムとして処理されない文字列（コメントと呼ばれます）を書くことができます
+
+# 以下の例にならって、文字を表示してください
+# ex. hello world と表示する
+print('hello world')
+
+# 1. Pastafarianism Flying Spaghetti Monsterism　と表示する
+# 2. 1234 と表示する
+# 3. Monty Python's Flying Circus と表示する (シングルクォーター' に注意)
