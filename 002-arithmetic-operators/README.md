14cd9383b841626c464af1a6708aa01263a0ec38
Takahiro Yamashita <takahiro.yamashita@uzabase.com>
2016-06-27 17:59:12 +0900

    002-arithmetic-operators

---
+++ 002-arithmetic-operators/README.md
@@ -0,0 +1,23 @@
+# Arithmetic Operators
+
+`main.py` のコメントの通りに算術演算子を用いて、コンピューターに計算をさせ、結果を表示してください
+
+以下の通りに表示されれば正しく動作しています
+
+```
+2
+Addition
+17
+Subtraction
+3
+Multiplication
+70
+Division
+1.4285714285714286
+Modulus
+3
+Exponent
+10000000
+Floor Division
+1
+```
---
+++ 002-arithmetic-operators/main.py
@@ -0,0 +1,20 @@
+# 行頭に # を書くと、プログラムとして処理されない文字列（コメントと呼ばれます）を書くことができます
+
+# 以下の例にならって、文字を表示してください
+# ex. 1 + 1 の結果を表示する
+print(1 + 1)
+
+# 1. 10 + 7 の結果を表示する
+print('Addition')
+# 2. 10 - 7 の結果を表示する
+print('Subtraction')
+# 3. 10 * 7 の結果を表示する
+print('Multiplication')
+# 3. 10 / 7 の結果を表示する
+print('Division')
+# 4. 10 % 7 の結果を表示する
+print('Modulus')
+# 5. 10 ** 7 の結果を表示する
+print('Exponent')
+# 6. 10 // 7 の結果を表示する
+print('Floor Division')
